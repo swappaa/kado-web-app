a17450c6bc8720ff7b36261ce057d91d9f9c70df
Larry Emol <larrydaveemol@gmail.com>
2021-01-27 00:24:00 +0800

    add amplufy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Aux from './hoc/Auxi/Auxi';
 import Layout from './hoc/Layout/Layout';
 import Home from './containers/Home/Home';
 import signupTalent from './containers/SignupTalent/SignupTalent';
-import talentProfiles from './containers/TalentProfile/TalentProfile';
+import talentProfile from './containers/TalentProfile/TalentProfile';
 
 
 class App extends Component {
@@ -15,7 +15,7 @@ class App extends Component {
       <Aux>
         <Layout>
           <Switch>
-            <Route path="/talent-profile" component={talentProfiles} />
+            <Route path="/talent-profile" component={talentProfile} />
             <Route path="/signup-talent" component={signupTalent} />
             <Route path="/" exact component={Home} />
             <Redirect to="/" />
