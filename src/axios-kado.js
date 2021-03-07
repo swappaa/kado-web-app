1534d7b985db4ce114471624ef66f3473ab610a3
swappaa <55630533+swappaa@users.noreply.github.com>
2021-03-07 11:42:41 +0800

    cors error

--- src/axios-kado.js
+++ src/axios-kado.js
@@ -1,7 +1,38 @@
 import axios from 'axios';
 
+const queryParams = 'maryapick';
+const tokens = 'eyJraWQiOiJMTVhYTUxFM1ZVOHg5MTh4WEx0cEVmWjdmbnVJYmJEaHlXcmRPeVR5aGtFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI5MmQ4MWU4MS05YmIzLTQ2NWQtOWUyMi01NTAyNWU0MmU3N2QiLCJldmVudF9pZCI6IjliMDIwOWVlLTY0YWEtNGQxZi1iNDA0LWZmZjUxMjlhMDhiNSIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2MTUwODQ1OTQsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy13ZXN0LTIuYW1hem9uYXdzLmNvbVwvdXMtd2VzdC0yX1RHZHNqS2dSVCIsImV4cCI6MTYxNTE3MDk5NCwiaWF0IjoxNjE1MDg0NTk0LCJqdGkiOiJlOThlYTAzNi1lZDM0LTQyNjEtYjJlZi1lYjgzNTAzOWJhMTkiLCJjbGllbnRfaWQiOiIzcXV2MmJkNXFqNGQzNG9xaTExdW9zdGlrNiIsInVzZXJuYW1lIjoidGFsZW50X3Rlc3QifQ.kEgytGydfJJUKelJZXyA-3oPIZrl6gJYHGBPW1b75HIYXs3dHd3JHM2LD_t3G8A_fSOKmecokoO9YrCHS7cxdr1UR5X_Ql1Q0zZdMKTEIK7JhRXQjqtnnT1P2V6nPzlYEaRLF6BtEhJcPZfh1Z79ldCoDoLJRGJu_2dx59gVCyyG88zj_PbdfP5WhG7aRFj0gGZ0czkRtu6P0X-q8Zu7kAGveYPmZzb2CgSg9KAI369P5WTLdeMosl0I6YrgNWWCSNvMBt3IGsJ3Hv3fmL8EpQpNcMswtkRCYB0GJwX4TXhclIAW5Gc-1vkq8ltB7SkZbRgXZv2gx8r1ILip0Xl2GA';
+
 const instance = axios.create({
-    baseURL: 'https://atln2zned9.execute-api.us-west-2.amazonaws.com/dev/'
+    baseURL: 'https://atln2zned9.execute-api.us-west-2.amazonaws.com/dev/',
 });
+
+instance.defaults.headers.common.Accept = 'application/json';
+instance.defaults.headers['Content-Type'] = 'multipart/form-data';
+instance.interceptors.request.use(async function (config) {
+    config.headers.common['Access-Control-Allow-Origin'] = '*';
+    config.headers.common['Access-Control-Allow-Headers'] = 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token';
+    config.headers.common['Access-Control-Allow-Credentials'] = true;
+    config.headers.common['Access-Control-Allow-Method'] = 'OPTIONS,POST,GET';
+    config.headers.common['Content-Type'] = 'application/json';
+    if (queryParams) {
+        config.headers.common.username = queryParams;
+        config.headers.common.access_token = tokens;
+    }
+    return config;
+});
+
+axios.interceptors.response.use(
+    function (response) {
+        console.log('>>>>>> API CALL: ', response.config.url);
+        // console.log('>>>>>> RESPONSE: ', response.data);
+        return Promise.resolve(response.data);
+    },
+    function (error) {
+        console.log('>>>>>> API CALL: ', error.response.config.url);
+        // console.log('>>>>>> ERROR: ', error.response.data);
+        return Promise.resolve(error.response.data.message);
+    }
+);
 
 export default instance;
--- src/store/actions/talent.js
+++ src/store/actions/talent.js
@@ -26,13 +26,7 @@ export const fetchTalent = (talentId, accessToken) => {
         dispatch(fetchTalentStart());
         const queryParams = talentId;
 
-        const config = {
-            headers: { 'username': queryParams, 'access_token': 'eyJraWQiOiJMTVhYTUxFM1ZVOHg5MTh4WEx0cEVmWjdmbnVJYmJEaHlXcmRPeVR5aGtFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJjZTdiZWNlYi0yYTI5LTQ2ZTAtOWU1NC04NjJhMGU1NTM1YjIiLCJldmVudF9pZCI6IjdjMTdjNWJhLWEyOWQtNDMxYS05OWVhLTczOTgyNDgxODEwZiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2MTQ4Njg0OTIsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy13ZXN0LTIuYW1hem9uYXdzLmNvbVwvdXMtd2VzdC0yX1RHZHNqS2dSVCIsImV4cCI6MTYxNDk1NDg5MiwiaWF0IjoxNjE0ODY4NDkyLCJqdGkiOiI0NzJkMWM0NC0yOTQzLTRhOGEtOTg4MS0zNjJjZGVlMDNjNmIiLCJjbGllbnRfaWQiOiIzcXV2MmJkNXFqNGQzNG9xaTExdW9zdGlrNiIsInVzZXJuYW1lIjoiZmFuX3Rlc3QifQ.M5nWOKTwExiOtzM4tpBuuLw1g0x-d5VmKOBY55JvKkRNgf-gqMkRu_fu27y2buBTxJnqGrMycDL9xCa-8gqA9sNW3hlOExk7B6LvujZ692Ygz9TxtIiAramVlATXNdDf3YO2GPzdB7fkT3_aqPJkkbyQcHYmaGRSWaw3Jl_cB4ux8muwH64NR-9JS_tKFAr9mSrt1UEuj_bJI78NUk5uP0F2wzMBVqNXshhoCcKRGvNZS-fFHNA5rzGi7KzMxowkd5dIPUhYvTCBe8b03nUTsaWnvIQyS2uGJfhjWQEf4rVOT7n87GcwAKw1OiXe5BgoW8nx0t4jvzbYBIGigfmgig' }
-        }
-
-        console.log(config);
-
-        axios.get('/talent/' + queryParams, config)
+        axios.get(`talent/${queryParams}`)
             .then(async res => {
                 const fetchedTalent = await res.data;
                 dispatch(fetchTalentSuccess(fetchedTalent));
